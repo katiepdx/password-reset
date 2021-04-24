e23ae408611a90043eaa2bf1a936105de18e02f3
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-04-23 19:55:53 -0700

    add login route and tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,15 +2,39 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('password-reset routes', () => {
   beforeEach(() => {
-    return setup(pool);
+    return setup(pool)
+      // add a user to db
+      .then(() => {
+        UserService.signup({
+          email: 'user1@user1.com',
+          password: 'user1password'
+        });
+      });
   });
 
   it('adds a user to to the database with an email and hashed password', () => {
     return request(app)
       .post('/api/v1/auth/signup')
+      .send({
+        email: 'user2@user2.com',
+        password: 'user1password'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: '2',
+          email: 'user2@user2.com',
+          password_hash: expect.any(String)
+        });
+      });
+  });
+
+  it('logins a user1', () => {
+    return request(app)
+      .post('/api/v1/auth/login')
       .send({
         email: 'user1@user1.com',
         password: 'user1password'
@@ -21,6 +45,42 @@ describe('password-reset routes', () => {
           email: 'user1@user1.com',
           password_hash: expect.any(String)
         });
+      });
+  });
+
+  it('tests a failed login attempt by user1 with incorrect password', () => {
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'user1@user1.com',
+        password: 'wrong password'
+      })
+      .then(res => {
+        expect(res.text).toEqual('Sorry, incorrect email/password');
+      });
+  });
+
+  it('tests a failed login attempt by user1 with incorrect password', () => {
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'user1@user1.com',
+        password: 'wrong password'
+      })
+      .then(res => {
+        expect(res.text).toEqual('Sorry, incorrect email/password');
+      });
+  });
+
+  it('tests a failed login attempt by user1 with incorrect email', () => {
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'user2@user1.com',
+        password: 'user1password'
+      })
+      .then(res => {
+        expect(res.text).toEqual('Sorry, incorrect email/password');
       });
   });
 });
