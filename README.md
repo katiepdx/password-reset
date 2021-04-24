45dc86a7af00ed8fe58afa54c9c41ddaf41c6e3f
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-04-24 13:57:01 -0700

    add forgot-password and reset-password routes and test

---
+++ README.md
@@ -0,0 +1,1 @@
+Referenced [this](https://dev.to/anthonyamaro15/how-to-implement-reset-password-functionality-with-node-postgresql-knex-n8a) article for resetting a password. Thanks!
--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,14 +4,19 @@ const request = require('supertest');
 const app = require('../lib/app');
 const UserService = require('../lib/services/UserService');
 
-describe('password-reset routes', () => {
+// mock sendgrid email service
+jest.mock('../lib/services/EmailService.js');
+const EmailService = require('../lib/services/EmailService');
+
+describe('sign up and login routes', () => {
   beforeEach(() => {
+    // drop db
     return setup(pool)
       // add a user to db
       .then(() => {
         UserService.signup({
-          email: 'user1@user1.com',
-          password: 'user1password'
+          email: process.env.TEST_USER_EMAIL_1,
+          password: 'test-email-1-pw'
         });
       });
   });
@@ -20,13 +25,13 @@ describe('password-reset routes', () => {
     return request(app)
       .post('/api/v1/auth/signup')
       .send({
-        email: 'user2@user2.com',
-        password: 'user1password'
+        email: process.env.TEST_USER_EMAIL_2,
+        password: 'test-email-2-pw'
       })
       .then(res => {
         expect(res.body).toEqual({
           id: '2',
-          email: 'user2@user2.com',
+          email: process.env.TEST_USER_EMAIL_2,
           password_hash: expect.any(String)
         });
       });
@@ -36,13 +41,13 @@ describe('password-reset routes', () => {
     return request(app)
       .post('/api/v1/auth/login')
       .send({
-        email: 'user1@user1.com',
-        password: 'user1password'
+        email: process.env.TEST_USER_EMAIL_1,
+        password: 'test-email-1-pw'
       })
       .then(res => {
         expect(res.body).toEqual({
           id: '1',
-          email: 'user1@user1.com',
+          email: process.env.TEST_USER_EMAIL_1,
           password_hash: expect.any(String)
         });
       });
@@ -52,19 +57,7 @@ describe('password-reset routes', () => {
     return request(app)
       .post('/api/v1/auth/login')
       .send({
-        email: 'user1@user1.com',
-        password: 'wrong password'
-      })
-      .then(res => {
-        expect(res.text).toEqual('Sorry, incorrect email/password');
-      });
-  });
-
-  it('tests a failed login attempt by user1 with incorrect password', () => {
-    return request(app)
-      .post('/api/v1/auth/login')
-      .send({
-        email: 'user1@user1.com',
+        email: process.env.TEST_USER_EMAIL_1,
         password: 'wrong password'
       })
       .then(res => {
@@ -76,11 +69,40 @@ describe('password-reset routes', () => {
     return request(app)
       .post('/api/v1/auth/login')
       .send({
-        email: 'user2@user1.com',
-        password: 'user1password'
+        // email 2 as wrong email
+        email: process.env.TEST_USER_EMAIL_2,
+        password: 'test-email-1-pw'
       })
       .then(res => {
         expect(res.text).toEqual('Sorry, incorrect email/password');
+      });
+  });
+});
+
+describe('password reset tests', () => {
+  it('tests that a user is sent a password reset link and given a 200 status code', () => {
+    return request(app)
+      .patch('/api/v1/auth/forgot-password')
+      .send({
+        email: process.env.TEST_USER_EMAIL_1,
+      })
+      .then((res) => {
+        expect(res.body.message).toEqual('Please check your email for a reset link!');
+        expect(res.status).toEqual(200);
+        expect(EmailService.sendEmail).toHaveBeenCalledTimes(1);
+      });
+  });
+
+  it.skip('tests that a 401 error is given if the token is incorrect', () => {
+    return request(app)
+      .patch('/api/v1/auth/reset-password/s8ds32jif8s')
+      .send({
+        email: process.env.TEST_USER_EMAIL_1,
+        password: 'newestpassword'
+      })
+      .then((res) => {
+        expect(res.body.message).toEqual('Sorry, please try again. Token may have expired or is incorrect');
+        expect(res.status).toEqual(401);
       });
   });
 });
