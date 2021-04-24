0b6dc6ad4ddff50cbd724f08c79ba4a311567aa8
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-04-23 18:38:20 -0700

    add signup route and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,20 @@ describe('password-reset routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('adds a user to to the database with an email and hashed password', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'user1@user1.com',
+        password: 'user1password'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: '1',
+          email: 'user1@user1.com',
+          password_hash: expect.any(String)
+        });
+      });
+  });
 });
